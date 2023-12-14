6b4cd8bb112de0d7942310daff35b56eb5e56673
Sk-Spark <skkumartheking@gmail.com>
2023-12-14 14:52:45 +0000

    ui complete...??

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,23 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios, { AxiosResponse, AxiosError } from 'axios';
+import { FiRefreshCcw  } from "react-icons/fi";
+
 
 function App() {
+
+  const [iotService, setIotService] = useState<[]>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadData, setLoadData] = useState(true); 
+
+  interface ApiResponse {
+    // Define the structure of your API response here
+    data: any;
+  }
+
+  async function fetchData(apiUrl: string): Promise<ApiResponse> {
+    try {
+      const response: AxiosResponse = await axios.get(apiUrl);
+      // Assuming your API returns JSON data, you can access it using response.data
+      return { data: response.data };
+    } catch (error: any) {
+      // Handle errors here
+      if (axios.isAxiosError(error)) {
+        // Axios error
+        const axiosError: AxiosError = error;
+        console.error('Axios error:', axiosError.message);
+        throw axiosError;
+      } else {
+        // Other errors
+        console.error('Error:', error.message);
+        throw error;
+      }
+    }
+  }
+
+  useEffect(()=>{
+    const apiUrl = '/api/mdns'; // Replace with your API endpoint
+    fetchData(apiUrl)
+    .then((result: ApiResponse) => {
+      console.log('API Response:', result.data);
+      setIotService(result.data);
+      setIsLoading(false);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      setIsLoading(false);
+    });
+  },[loadData]);
+
+  const getIotService = ()=>{
+    return iotService?.map((i:any)=>{
+      return <>
+        <a className="App-link"
+          target="_blank"
+          href={`http://${i.IP}`}>{`${i.hostName}`}</a>
+      </>
+    });
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello from RPi Server.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='header_div'>
+          <h2>RPi IOT Server</h2>
+          <div className='referesh_div' onClick={()=>{
+              setLoadData(!loadData);
+              setIsLoading(true);
+            }}> 
+            <FiRefreshCcw className={isLoading?'App-logo':''} />
+          </div>
+        </div>
+        {getIotService()}
       </header>
     </div>
   );
